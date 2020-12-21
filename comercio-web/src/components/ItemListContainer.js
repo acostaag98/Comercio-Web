f6a3cfd59ceb2af16f17c1aef182f5e0b8e43fa0
acostaag98 <acostaag98@gmail.com>
2020-12-21 11:01:16 -0300

    Correcciones entrega final

--- comercio-web/src/components/ItemListContainer.js
+++ comercio-web/src/components/ItemListContainer.js
@@ -9,7 +9,6 @@ function ItemListContainer() {
     useEffect(() => {
         const db = getFirestore();
         const itemCollection = db.collection('items');
-        const ofertas = itemCollection.where('category', '==', category)
         if (category === undefined) {
             itemCollection.get().then((querySnapshot) => {
                 if(querySnapshot.size === 0) {
@@ -19,6 +18,7 @@ function ItemListContainer() {
             });
         }
         else{ 
+            const ofertas = itemCollection.where('category', '==', category)
             ofertas.get().then((querySnapshot) => {
                 if(querySnapshot.size === 0) {
                     console.log('no results')
